7a1ab5a224f628dbd863bb293cedd3aa2950382a
facundobn98 <facundobrun1998@gmail.com>
2025-09-25 20:54:44 -0300

    progreso de lista de compras

---
+++ challenge.py
@@ -0,0 +1,17 @@
+
+class Solution(object):
+    def twoSum(self, nums=list, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+
+def arrayRunner(target, nums):
+    respuesta = 0
+    while respuesta != target or i >= len.(nums):
+        
+        i+=1
+        while 
+
